0cc5d66552360b6cc496ea087ffb387dadbe9a13
Thailan Gouvea <thailangm@icloud.com>
2024-09-25 10:40:02 +0300

    Implements collapsable menu on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
 import Styles from "./styles.module.css";
 
 export default function App() {
+  const [menuToggle, setMenuToggle] = useState(false);
   return (
     <div className={Styles.App}>
       <div className={Styles.topContainer}>
         <div className={Styles.GlobalContainer}>
-          <span>=</span>Global Menu
+          <span
+            onClick={() => {
+              setMenuToggle(!menuToggle);
+            }}
+          >
+            =
+          </span>
+          Global Menu
         </div>
         <div className={Styles.HeaderContainer}>Header</div>
       </div>
+      <nav
+        className={Styles.listContainer}
+        style={{ display: menuToggle ? "block" : "" }}
+      >
+        <ul>
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+          <li>Item 4</li>
+        </ul>
+      </nav>
       <div className={Styles.HeaderContainer2}>Header</div>
       <div className={Styles.GlobalContainer2}>Global Menu</div>
       <div className={Styles.AdsContainer}>Ads</div>
